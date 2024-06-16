ef963fac0ed16b6b8c4893b6a1a7f2ac7aa502a5
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-06-16 18:46:19 -0300

    Added environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sanity from "astro-sanity"
+import { DATASET, PROJECT_ID } from './src/lib/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        sanity({
+            projectId: PROJECT_ID,
+            dataset: DATASET,
+            apiVersion: "2021-03-25",
+            useCdn: true
+        })
+    ],
+});
