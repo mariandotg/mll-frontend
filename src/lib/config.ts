5a1e76d0754f93c69ef7c5c6bfe76ecd3d2a3c1f
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-06-16 23:07:35 -0300

    Modified env variables on config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,14 @@
-export const { PROJECT_ID, DATASET } = process.env;
+import { loadEnv } from "vite";
+export const {
+  PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  PUBLIC_SANITY_STUDIO_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  PUBLIC_SANITY_DATASET,
+} = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
+export const projectId =
+  PUBLIC_SANITY_STUDIO_PROJECT_ID! ||
+  PUBLIC_SANITY_PROJECT_ID!;
+export const dataset =
+  PUBLIC_SANITY_STUDIO_DATASET! ||
+  PUBLIC_SANITY_DATASET!;
