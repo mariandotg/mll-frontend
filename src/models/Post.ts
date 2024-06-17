6144b7921aa2f4a72d73f23554ca38324e725047
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-06-17 20:25:38 -0300

    Updated Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,8 +1,27 @@
 interface Slug {
+    _type: string,
     current: string
+}
+
+interface Asset {
+    _ref: string;
+    _type: string;
+}
+
+interface Image {
+    _type: string,
+    asset: Asset
 }
 
 export interface Post {
     title: string;
     slug: Slug;
+    category: string;
+    body: string;
+    imageUrl: Image;
+    _createdAt: string;
+    _updatedAt: string;
+    _rev: string;
+    _type: string;
+    _id: string;
 }
