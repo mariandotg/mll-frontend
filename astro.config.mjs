3e7e8f78a2c46a0814665deffddd5e5781b27a80
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-06-17 13:33:16 -0300

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import sanityIntegration from "@sanity/astro";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import { loadEnv } from "vite";
-const { PUBLIC_SANITY_STUDIO_PROJECT_ID, PUBLIC_SANITY_STUDIO_DATASET } = loadEnv( process.env.NODE_ENV, process.cwd(), '');
+import tailwind from "@astrojs/tailwind";
+const {
+  PUBLIC_SANITY_STUDIO_PROJECT_ID,
+  PUBLIC_SANITY_STUDIO_DATASET
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +18,7 @@ export default defineConfig({
     dataset: PUBLIC_SANITY_STUDIO_DATASET,
     apiVersion: "2021-03-25",
     useCdn: false,
-    studioBasePath: '/admin',
-    
-  }), react()],
+    studioBasePath: '/admin'
+  }), react(), tailwind()],
   adapter: netlify()
 });
